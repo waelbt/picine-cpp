03493ce3e18e281555c725913181cca8d416f21f
waelbt <boutzougarte@gmail.com>
2022-10-20 17:05:59 +0100

    ex01 cpp07

--- module07/ex01/Iter.hpp
+++ module07/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: waboutzo <waboutzo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 17:17:22 by waboutzo          #+#    #+#             */
-/*   Updated: 2022/10/19 17:50:33 by waboutzo         ###   ########.fr       */
+/*   Updated: 2022/10/20 16:53:38 by waboutzo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,16 @@
 #include <iostream>
 
 template <typename T>
-void Iter(T* ar, size_t len, (void) f(T))
+void Iter(T* ar, size_t len, void (*f) (T&))
+{
+	for(size_t i = 0; i < len; i++)
+		f(ar[i]);
+}
+
+template <typename T>
+void f(T& n)
+{
+	n += 13;
+}
+
 #endif
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: waboutzo <waboutzo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 17:17:26 by waboutzo          #+#    #+#             */
-/*   Updated: 2022/10/19 17:18:47 by waboutzo         ###   ########.fr       */
+/*   Updated: 2022/10/20 17:04:20 by waboutzo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 int main(void)
 {
+	{
+		int ar[5] = {16, 2, 77, 40, 233};
+
+		Iter<int>(ar, 5, &f<int>);
+		for(int i = 0; i < 5; i++)
+			std::cout << ar[i] <<", ";
+		std::cout << std::endl;
+	}
+	{
+		double ar[5] = {16.3, 2.0, 77.3, 40.44, 233.55};
+
+		Iter<double>(ar, 5, &f<double>);
+		for(int i = 0; i < 5; i++)
+			std::cout << ar[i] <<", ";
+		std::cout << std::endl;
+	}
 	return 0;
 }
